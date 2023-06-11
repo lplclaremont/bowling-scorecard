456dc0944016d884453c12f8a1eb33ce63fe39cd
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-06-11 19:47:39 +0100

    scorecard component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {
-  useNavigate,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Scorecard from './Scorecard'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Route path="/scorecard">
-      <Scorecard />
-    </Route>
+    <Router>
+      <Routes>
+        <Route path="/scorecard" element={<Scorecard />} />
+      </Routes>
+    </Router>
   )
 }
-
-{/* <>
-<div>
-  <a href="https://vitejs.dev" target="_blank">
-    <img src={viteLogo} className="logo" alt="Vite logo" />
-  </a>
-  <a href="https://react.dev" target="_blank">
-    <img src={reactLogo} className="logo react" alt="React logo" />
-  </a>
-</div>
-<h1>Vite + Boomba</h1>
-<div className="card">
-  <button onClick={() => setCount((count) => count + 1)}>
-    count is {count}
-  </button>
-  <p>
-    Edit <code>src/App.jsx</code> and save to test HMR
-  </p>
-</div>
-<p className="read-the-docs">
-  Click on the Vite and React logos to learn more
-</p>
-</> */}
 
 export default App
---
+++ frontend/src/Scorecard.jsx
@@ -0,0 +1,5 @@
+const Scorecard = () => {
+  return <h1>Scorecard Time</h1>
+}
+
+export default Scorecard;
